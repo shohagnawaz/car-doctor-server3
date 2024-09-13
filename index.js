9d890950734fb2edc5962e69f77620d14fce60a5
shohagnawaz <snawazdigital@gmail.com>
2024-09-13 18:48:48 +0600

    update

--- index.js
+++ index.js
@@ -36,19 +36,48 @@ async function run() {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const options = {
-          projection: { title: 1, price: 1, service_id: 1 }
+          projection: { title: 1, price: 1, service_id: 1, img: 1 }
         };
         const result = await serviceCollection.findOne(query, options);
         res.send(result)
     });
     // booking-collection
+    app.get('/bookings', async(req, res) => {
+        console.log(req.query);
+        let query = {};
+        if(req.query?.email){
+          query = {email: req.query.email}
+        } 
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+    })
     app.post('/bookings', async(req, res) => {
         const booking = req.body;
         console.log(booking);
-        // const result = await bookingCollection.insertOne(booking);
-        // res.send(result)
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
         
     });
+    app.patch('/bookings/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = { _di: new ObjectId(id)};
+        const updateBooking = req.body;
+        console.log(updateBooking);
+        const updateDoc = {
+          $set: {
+            status: updateBooking.status
+          }
+        }
+        const result = await bookingCollection.updateOne(filter, updateDoc);
+        res.send(result); 
+    });
+    app.delete('/bookings/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
